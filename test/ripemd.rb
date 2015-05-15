3e257c626df1319b1af6b37cd2d7c41e3c3acbc2
Daniel Rodríguez <sadasant@gmail.com>
2015-05-15 17:23:27 -0500

    Added a simple test file

---
+++ test/ripemd.rb
@@ -0,0 +1,11 @@
+# RMD160 test
+
+if Object.const_defined?(:RMD160)
+  assert("RMD160") do
+    RMD160.class == Module
+  end
+  assert("hexdigest must work") do
+    msg = RMD160.hexdigest("something")
+    assert_equal(msg, "76202357772b26db74cf62e36efb6d69f68e48ab")
+  end
+end

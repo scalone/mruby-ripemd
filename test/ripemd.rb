43eb35a50843fb6835a1a812e0eecc72187ec4f4
Thiago Scalone <thiago@scalone.com.br>
2015-05-15 20:13:34 -0300

    break line between test.

--- test/ripemd.rb
+++ test/ripemd.rb
@@ -4,6 +4,7 @@ if Object.const_defined?(:RMD160)
   assert("RMD160") do
     RMD160.class == Module
   end
+
   assert("hexdigest must work") do
     msg = RMD160.hexdigest("something")
     assert_equal(msg, "76202357772b26db74cf62e36efb6d69f68e48ab")

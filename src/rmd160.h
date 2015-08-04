a756700169d5066fbdab53a5a8ed49904601c119
Thiago Scalone <thiago@scalone.com.br>
2015-08-03 23:29:18 -0300

    Remove immintrin dependency.

--- src/rmd160.h
+++ src/rmd160.h
@@ -3,7 +3,7 @@
  *      FILE:     rmd160.h
  *
  *      CONTENTS: Header file for a sample C-implementation of the
- *                RIPEMD-160 hash-function. 
+ *                RIPEMD-160 hash-function.
  *      TARGET:   any computer with an ANSI C compiler
  *
  *      AUTHOR:   Antoon Bosselaers, ESAT-COSIC
@@ -19,26 +19,15 @@
 #define  RMD160H
 
 /********************************************************************/
-#include <immintrin.h>
-#include <inttypes.h>
-/* typedef 8 and 32 bit types, resp.  */
-/* adapt these, if necessary, 
-   for your operating system and compiler */
-typedef    unsigned char        byte;
-typedef    unsigned int         dword;
 
-/* if this line causes a compiler error, 
-   adapt the defintion of dword above */
-typedef int the_correct_size_was_chosen [sizeof (dword) == 4? 1: -1];
+/* typedef 8 and 32 bit types, resp.  */
+/* adapt these, if necessary,
+   for your operating system and compiler */
+typedef    uint8_t        byte;   /* unsigned 8-bit integer */
+typedef    uint16_t       word;   /* unsigned 16-bit integer */
+typedef    uint32_t        dword;  /* unsigned 32-bit integer */
 
 /********************************************************************/
-static const uint32_t mdbuf_quad[5*4] = {
-		0x67452301, 0x67452301, 0x67452301, 0x67452301,
-		0xefcdab89, 0xefcdab89, 0xefcdab89, 0xefcdab89,
-		0x98badcfe, 0x98badcfe, 0x98badcfe, 0x98badcfe,
-		0x10325476, 0x10325476, 0x10325476, 0x10325476,
-		0xc3d2e1f0, 0xc3d2e1f0, 0xc3d2e1f0, 0xc3d2e1f0
-};
 
 /* macro definitions */
 
@@ -54,12 +43,12 @@ static const uint32_t mdbuf_quad[5*4] = {
 #define ROL(x, n)        (((x) << (n)) | ((x) >> (32-(n))))
 
 /* the five basic functions F(), G() and H() */
-#define F(x, y, z)        ((x) ^ (y) ^ (z)) 
-#define G(x, y, z)        (((x) & (y)) | (~(x) & (z))) 
+#define F(x, y, z)        ((x) ^ (y) ^ (z))
+#define G(x, y, z)        (((x) & (y)) | (~(x) & (z)))
 #define H(x, y, z)        (((x) | ~(y)) ^ (z))
-#define I(x, y, z)        (((x) & (z)) | ((y) & ~(z))) 
+#define I(x, y, z)        (((x) & (z)) | ((y) & ~(z)))
 #define J(x, y, z)        ((x) ^ ((y) | ~(z)))
-  
+
 /* the ten basic operations FF() through III() */
 #define FF(a, b, c, d, e, x, s)        {\
       (a) += F((b), (c), (d)) + (x);\
@@ -121,38 +110,21 @@ void MDinit(dword *MDbuf);
  *  initializes MDbuffer to "magic constants"
  */
 
-void MM_MDinit(uint32_t *MDbuf);
-/*
- *  initializes MDbuffer to "magic constants"
- */
-
 void compress(dword *MDbuf, dword *X);
 /*
  *  the compression function.
  *  transforms MDbuf using message bytes X[0] through X[15]
  */
-void MM_compress(__m128i *MDbuf, __m128i *X);
 
 void MDfinish(dword *MDbuf, byte *strptr, dword lswlen, dword mswlen);
 /*
- *  puts bytes from strptr into X and pad out; appends length 
+ *  puts bytes from strptr into X and pad out; appends length
  *  and finally, compresses the last block(s)
  *  note: length in bits == 8 * (lswlen + 2^32 mswlen).
  *  note: there are (lswlen mod 64) bytes left in strptr.
- */
-void MM_MDfinish(__m128i *MDbuf, __m128i *strptr, dword lswlen, dword mswlen);
-
-
-void MM_matrix_transpose_r2c(__m128i* inbuf,__m128i* outbuf, uint32_t rows, uint32_t colums);
-/*
- * uses uint32_t array as input and transposes the rows to colunms
- */
-
-void MM_matrix_transpose_c2r(__m128i* inbuf,__m128i* outbuf, uint32_t rows, uint32_t colums);
-/*
- * uses uint32_t array as input and transposes the columns to rows
  */
 
 #endif  /* RMD160H */
 
 /*********************** end of file rmd160.h ***********************/
+
